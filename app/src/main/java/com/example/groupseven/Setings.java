3150cce7ed7f57161afe71dddefb86477a181b87
tutujohn <103087402+tutujohn@users.noreply.github.com>
2023-07-06 16:58:18 +0000

    Update Setings.java

--- app/src/main/java/com/example/groupseven/Setings.java
+++ app/src/main/java/com/example/groupseven/Setings.java
@@ -1,6 +1,13 @@
 package com.example.groupseven;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Setings extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings);
+    }
 }
