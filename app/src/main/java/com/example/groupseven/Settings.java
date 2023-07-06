cac4fbfe7603084a4c7efcabc4862389fa390621
Richard Djirackor <rdjirackor@st.ug.edu.gh>
2023-07-06 19:56:29 +0000

    Added a toggle button

--- app/src/main/java/com/example/groupseven/Settings.java
+++ app/src/main/java/com/example/groupseven/Settings.java
@@ -5,8 +5,10 @@ import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
@@ -16,18 +18,24 @@ public class Settings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings);
-        TextView deleteAll, darkLight, about,setHead;
+        TextView deleteAll, darkLight, about,setHead,togLabel;
         DialogThing dialog = new DialogThing();
         ImageButton back=findViewById(R.id.backer);
+        ToggleButton toggle=findViewById(R.id.toggle);
 
         deleteAll = findViewById(R.id.delAll);
         darkLight = findViewById(R.id.darkLight);
         about = findViewById(R.id.about);
         setHead=findViewById(R.id.SettingsHeader);
+        togLabel=findViewById(R.id.toggleLabel);
 
         setHead.setTextColor(Color.BLACK);
         setHead.setTypeface(Typeface.defaultFromStyle(Typeface.BOLD));
-        setHead.setTextSize(17);
+        setHead.setTextSize(19);
+
+        togLabel.setTextColor(Color.BLACK);
+        togLabel.setTypeface(Typeface.defaultFromStyle(Typeface.BOLD));
+        togLabel.setTextSize(18);
 
 
         deleteAll.setTextColor(Color.BLACK);
@@ -41,6 +49,8 @@ public class Settings extends AppCompatActivity {
         about.setTextColor(Color.BLACK);
         about.setTypeface(Typeface.defaultFromStyle(Typeface.BOLD));
         about.setTextSize(20);
+
+
 
         back.setBackgroundColor(Color.TRANSPARENT);
 
@@ -59,9 +69,36 @@ public class Settings extends AppCompatActivity {
                 dialog.show(fragmentManager, "dialog");
             }
         });
+        toggle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle toggle button state change
+                if (toggle.isChecked()) {
+                    // Toggle is ON
+                    setAppThemeDark();
+                } else {
+                    // Toggle is OFF
+                    setAppThemeLight();
+                }
+            }
+        });
     }
     public void openCreateAccount(){
         Intent intent=new Intent(this,CreateAccount.class);
         startActivity(intent);
     }
+
+    private void setAppThemeDark() {
+        // Set background color to black for all pages
+        ViewGroup rootView = findViewById(android.R.id.content);
+        rootView.setBackgroundColor(Color.BLACK);
+    }
+
+    private void setAppThemeLight() {
+        // Set background color to white for all pages
+        ViewGroup rootView = findViewById(android.R.id.content);
+        rootView.setBackgroundColor(Color.WHITE);
+    }
 }
+
+
