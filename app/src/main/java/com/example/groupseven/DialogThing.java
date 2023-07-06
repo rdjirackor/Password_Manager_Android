bad263ba987946bc2d47ac54a872ee67c39c501a
tutujohn <103087402+tutujohn@users.noreply.github.com>
2023-07-06 18:21:51 +0000

    Created the Dialog

---
+++ app/src/main/java/com/example/groupseven/DialogThing.java
@@ -0,0 +1,27 @@
+package com.example.groupseven;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.fragment.app.DialogFragment;
+
+public class DialogThing extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage("Delete all Saved Passwords?!")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // Perform the action when "Yes" is clicked
+                    }
+                })
+                .setNegativeButton("Wait.. what, No!", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // Perform the action when "No" is clicked
+                    }
+                });
+        return builder.create();
+    }
+}
