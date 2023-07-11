8226646b851423e004a32cc5461b5c62d159df35
mrkoomson <102766511+mrkoomson@users.noreply.github.com>
2023-07-11 16:50:49 +0000

    Added the image back button for about page

--- app/src/main/java/com/example/groupseven/About.java
+++ app/src/main/java/com/example/groupseven/About.java
@@ -1,8 +1,12 @@
 package com.example.groupseven;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,8 +18,8 @@ public class About extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about);
-        TextView t1=findViewById(R.id.name);
-        TextView t2=findViewById(R.id.det);
+        TextView t1 = findViewById(R.id.name);
+        TextView t2 = findViewById(R.id.det);
 
         t1.setTextColor(Color.BLACK);
         t1.setTypeface(Typeface.defaultFromStyle(Typeface.ITALIC));
@@ -25,5 +29,18 @@ public class About extends AppCompatActivity {
         t2.setTypeface(Typeface.defaultFromStyle(Typeface.ITALIC));
         t2.setTextSize(20);
 
+
+        ImageButton backButton = findViewById(R.id.backr);
+        backButton.setBackgroundColor(Color.TRANSPARENT);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
+
+
+
